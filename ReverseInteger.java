1398c4f3bc2c57abc46dbe905fd65bc9981e2120
pavithrapalanisamy <p.pavithra12349@gmail.com>
2017-06-28 14:49:18 +0530

    Add files via upload

---
+++ ReverseInteger.java
@@ -0,0 +1,29 @@
+package GUVI;
+import java.util.Scanner;
+public class ReverseInteger {
+
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer that you would like to have reversed: ");
+        int number = input.nextInt();
+
+        reverse(number);
+    }
+
+    public static void reverse(int userInteger)
+    {
+        int tempDigit = 0;
+
+        while (userInteger > 0){
+
+            tempDigit = userInteger % 10;
+            System.out.print(tempDigit);
+            userInteger = userInteger / 10;
+        }
+    }
+
+	}
+
+
